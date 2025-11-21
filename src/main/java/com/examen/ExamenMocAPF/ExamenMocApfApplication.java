47305eaa99083b89e6258799106d6a5f5d7f101b
Alvaro Perez Fernandez <alvaropf36@educastur.es>
2025-11-21 17:29:47 +0100

    Initial commit

---
+++ src/main/java/com/examen/ExamenMocAPF/ExamenMocApfApplication.java
@@ -0,0 +1,13 @@
+package com.examen.ExamenMocAPF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMocApfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMocApfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/ExamenMocAPF/ExamenMocApfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.ExamenMocAPF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMocApfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
